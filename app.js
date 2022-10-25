34364dac5c4cbf0ff2743131b8f7d1d26fba7921
Lorivie Abapo <lorivieabapo@Lorivies-MacBook-Pro.local>
2022-10-25 10:46:10 -0600

    Fizz Buzz Answered

--- app.js
+++ app.js
@@ -10,12 +10,24 @@ for (let i=1; i < 21; i++) {
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-
+for (let i=0; i < 201; i+=2) {
+    console.log(i)
+}
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for (let i=1; i <100; i++) {
+    if (i % 3===0 && i % 5===0) {
+        console.log("FizzBuzz");
+    } else if (i % 5===0) {
+        console.log("Buzz");
+    }else if (i%3===0) {
+        console.log("Fizz")
+    } else {
+        console.log(i);
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
